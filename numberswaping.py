c4136eaeb8493ebb2d849d3cb1d9c6873c875405
kayla-13-blip <SholotanOdunayo13@gmail.com>
2025-10-26 19:19:19 +0000

    Add files via upload

---
+++ numberswaping.py
@@ -0,0 +1,21 @@
+
+x = 5
+
+y = 9
+
+z = 1
+
+print("Before sorting:")
+
+print("x =", x, "y =", y, "z =", z)
+
+# Step 1: Arrange in ascending order
+
+numbers = [x, y, z]
+
+numbers.sort()
+
+print("\nAfter arranging in ascending order:")
+
+print(numbers)
+
