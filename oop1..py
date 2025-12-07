7d471ed7dfa18dbe591e3e53d2922f5ffba012f1
kayla-13-blip <SholotanOdunayo13@gmail.com>
2025-12-07 19:06:18 +0000

    Add files via upload

---
+++ oop1..py
@@ -0,0 +1,11 @@
+class student:
+    Year = 10 
+    name = "Penguin"
+    def introduction(self):
+        print("Hi I am a student")
+    def details(self):
+            print("My name is", self.name)
+            print("I study in Year", self.Year)
+ob = student()
+ob.introduction()
+ob.details()
---
+++ oop1.py
@@ -0,0 +1,5 @@
+class student:
+    Year = 10
+    print(" Hi I am a student of year", Year  )
+
+oob = student() 
