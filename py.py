2be38f663846c4452cfb7d8a358d23e47144ae54
kayla-13-blip <SholotanOdunayo13@gmail.com>
2026-02-04 19:10:08 +0000

    Add files via upload

---
+++ py.py
@@ -0,0 +1,26 @@
+file_read = open('example.txt','r')
+print("File in Read Mode.")
+print(file_read.read())
+file_read.close()
+file = open('example.txt','r')
+print(file.read())
+file.close()
+file = open('example.txt','r')
+print("\n Read in parts\n")
+print(file.read(11))
+file.close()
+fn = open('answer.txt','r')
+fn1 = open('word.txt','w')
+cont = fn.readlines()
+type(cont)
+for i in range(1,len(cont)+1):
+    if(i %2 !=0):
+        fn1.write(cont[i-1])
+    else:
+        pass
+fn1.close()  
+fn1 = open('word.txt','r') 
+cont1 = fn1.read()
+print(cont1)   
+fn.close() 
+fn1.close()
